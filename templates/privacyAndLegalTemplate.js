d7f9925e9cc3995fa9107fe5e247cdc0da580998
Cici403 <christian.schreter@gmail.com>
2024-12-02 23:18:44 +0100

    added js template and script for the privacy_and_legal html sides

---
+++ templates/privacyAndLegalTemplate.js
@@ -0,0 +1,24 @@
+function getPrivacyPolicyTemplate(){
+    return `
+    <div class="privacy_and_legal_privacy_plolicy_text_container">
+
+        <h1>Privacy Policy</h1>
+        <p class="privacy_and_legal_privacy_plolicy_text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim
+            justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis
+            ipsum, condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor
+            dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et,
+            imperdiet semper justo.
+        </p>
+
+        <h1>Privacy Policy</h1>
+        <p class="privacy_and_legal_privacy_plolicy_text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque odio felis, iaculis ut massa eget, ornare lacinia urna. In dignissim
+            justo eu velit sagittis, in scelerisque nulla convallis. Vestibulum eros lorem, sollicitudin eget eros non, varius aliquam mauris. Sed turpis
+            ipsum, condimentum quis nulla at, lobortis facilisis ipsum. Nunc erat justo, hendrerit vel enim vitae, feugiat mattis dui. In auctor
+            dignissim luctus. Mauris ornare ipsum at ultrices eleifend. Praesent tempus congue magna. Quisque libero erat, pharetra a neque et,
+            imperdiet semper justo.
+        </p>    
+        </div>
+    `
+}
