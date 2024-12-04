d678157019c37219e36d7b4677e201d1a876d657
Cici403 <christian.schreter@gmail.com>
2024-12-04 19:00:11 +0100

    load data from firebase is in the script js

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 let users = [];
 const BASE_URL = "https://join-bbd82-default-rtdb.europe-west1.firebasedatabase.app/";
+
+/**
+ * Load user data from Firebase Realtime Database into global users array
+ */
+async function onloadFunction() {
+    let userResponse = await loadData("users");
+    let userKeyArray = Object.keys(userResponse);
+
+    for (let index = 0; index < userKeyArray.length; index++) {
+        let userEntries = Object.values(userResponse[userKeyArray[index]]);
+        for (let entry of userEntries) {
+            users.push(entry);
+        }
+    }
+    console.log("Global users array:", users);
+}
+
+/**
+ * Fetch data from Firebase Realtime Database
+ * users is the path to the data in the database 
+ * 
+ */
+async function loadData(path = "") {
+    let response = await fetch(BASE_URL + path + ".json");
+    let responseToJson = await response.json();
+    return responseToJson;
+}
+
