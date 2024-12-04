532d9962c460cca2c919aeabdb31eee55f12e839
MuhammedTuerkel <muhammedturkel@icloud.com>
2024-12-04 17:45:08 +0100

    Greeting auf die Tageszeit angepasst

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,4 +1,22 @@
+let greeting;
+
+
 function showSubmenu() {
     document.getElementById('Submenu').style = "";
-    document.getElementById('help-user').style = "Display:none;";
+
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let today = new Date();
+    let hourNow = today.getHours();
+    if (hourNow >= 0 && hourNow < 12) {
+        greeting = "Good Morning"
+    }
+    else if (hourNow >= 12 && hourNow < 18) {
+        greeting = "Good Afternoon"
+    }
+    else if (hourNow >= 18 && hourNow < 24) {
+        greeting = "Good Evening"
+    }
+document.getElementById('greeting').innerHTML = greeting;
+});
