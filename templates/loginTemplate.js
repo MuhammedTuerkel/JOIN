4c3d1ac249308b4b95318578a403adabcee7b697
Cici403 <christian.schreter@gmail.com>
2024-12-08 18:37:45 +0100

    added at the signup Template the error class if the mail is used warning

--- templates/loginTemplate.js
+++ templates/loginTemplate.js
@@ -37,49 +37,48 @@ function getUserLoginTemplate() {
  */
 function getSignUpTemplate() {
     return `
-<form id="logiSignUp" class="signup_card scale_up_center" onsubmit="pushNewUserinFireBaseArray(event)">
-    <div class="login_back_to_sign_in">
-        <img onclick="backToLogIn()" src="./assets/img/back_arrow.png" alt="Zurück zur Anmeldung">
-    </div>
-    <div class="login_sign_up_card_headline">
-        <h1>Sign up</h1>
-        <div class="login_underline"></div>
-    </div>
-    <div class="login_input_field">
-        <input id="signUpName" type="text" placeholder="Name" class="blur_placeholder" oninput="validateSignUpForm(); checkSignUpNameInput()">
-        <img src="./assets/img/person.png" alt="Person Icon">
-        <small id="signUpNameError" class="login_input_mail_info login_d_none">Bitte geben Sie einen gültigen Namen ein (nur Buchstaben und Leerzeichen).</small>
-    </div>
-    <div class="login_input_field">
-        <input id="signUpInputMail" type="text" placeholder="Mail" oninput="validateSignUpForm(); checkSignUpEmailInput()" class="blur_placeholder" required>
-        <img src="./assets/img/mail.png" alt="Mail Icon">
-        <small id="signUpInputMailError" class="login_input_mail_info login_d_none">Bitte geben Sie eine gültige E-Mail-Adresse ein.</small>
-        <small id="signUpInputMailExistsError" class="login_input_mail_info login_d_none">Diese E-Mail-Adresse ist bereits registriert.</small>
-    </div>
-    <div class="login_input_field">
-        <input id="signUpInputPassword" type="password" placeholder="Passwort" oninput="validateSignUpForm(); toggleSignUpPasswordIcon(); checkPasswordInput()" class="blur_placeholder" required>
-        <img id="togglePasswordIconSignUp" src="./assets/img/lock.png" alt="Show/Hide Password" onclick="toggleSignUpPasswordVisibility()">
-        <small id="signUpPasswordError" class="login_input_mail_info login_d_none">Das Passwort muss mindestens 3 Zeichen lang sein.</small>
-    </div>
-    <div class="login_input_field">
-        <input id="signUpConfirmInputPassword" type="password" placeholder="Confirm Passwort" oninput="validateSignUpForm(); toggleAndCheckInputConfirmPassword()" class="blur_placeholder" required>
-        <img id="togglePasswordIconSignUpConfirm" src="./assets/img/lock.png" alt="Show/Hide Password" onclick="toggleSignUpConfirmPasswordVisibility()">
-        <small id="signUpConfirmPasswordError" class="login_input_mail_info login_d_none">Die Passwörter stimmen nicht überein.</small>
-    </div>
-    <div class="login_checkbox_container">
-        <div onclick="acceptTerms()" class="login_checkbox">
-            <input type="checkbox" id="acceptTerms" class="form_check_input" disabled>
-        </div> 
-        <div class="login_checkbox">
-            <label for="acceptTerms" class="form-check-label">I accept the <p onclick="showPrivacyPolicy()" class="hover_target">Privacy policy</p></label>
+    <form id="logiSignUp" class="signup_card scale_up_center" onsubmit="pushNewUserinFireBaseArray(event)">
+        <div class="login_back_to_sign_in">
+            <img onclick="backToLogIn()" src="./assets/img/back_arrow.png" alt="Zurück zur Anmeldung">
         </div>
-    </div>
-    <div class="signup_buttons">
-        <button class="login_signup_buttons_styles" id="signUpButton" disabled>
-            Sign up
-        </button>
-    </div>
-</form>
-
+        <div class="login_sign_up_card_headline">
+            <h1>Sign up</h1>
+            <div class="login_underline"></div>
+        </div>
+        <div class="login_input_field">
+            <input id="signUpName" type="text" placeholder="Name" class="blur_placeholder" oninput="validateSignUpForm(); checkSignUpNameInput()">
+            <img src="./assets/img/person.png" alt="Person Icon">
+            <small id="signUpNameError" class="login_input_mail_info login_d_none">Bitte geben Sie einen gültigen Namen ein (nur Buchstaben und Leerzeichen).</small>
+        </div>
+        <div class="login_input_field">
+            <input id="signUpInputMail" type="text" placeholder="Mail" oninput="validateSignUpForm(); checkSignUpEmailInput()" class="blur_placeholder" required>
+            <img src="./assets/img/mail.png" alt="Mail Icon">
+            <small id="signUpInputMailError" class="login_input_mail_info login_d_none">Bitte geben Sie eine gültige E-Mail-Adresse ein.</small>
+            <small id="signUpInputMailExistsError" class="login_input_mail_info login_d_none">Diese E-Mail-Adresse ist bereits registriert.</small>
+        </div>
+        <div class="login_input_field">
+            <input id="signUpInputPassword" type="password" placeholder="Passwort" oninput="validateSignUpForm(); toggleSignUpPasswordIcon(); checkPasswordInput()" class="blur_placeholder" required>
+            <img id="togglePasswordIconSignUp" src="./assets/img/lock.png" alt="Show/Hide Password" onclick="toggleSignUpPasswordVisibility()">
+            <small id="signUpPasswordError" class="login_input_mail_info login_d_none">Das Passwort muss mindestens 3 Zeichen lang sein.</small>
+        </div>
+        <div class="login_input_field">
+            <input id="signUpConfirmInputPassword" type="password" placeholder="Confirm Passwort" oninput="validateSignUpForm(); toggleAndCheckInputConfirmPassword()" class="blur_placeholder" required>
+            <img id="togglePasswordIconSignUpConfirm" src="./assets/img/lock.png" alt="Show/Hide Password" onclick="toggleSignUpConfirmPasswordVisibility()">
+            <small id="signUpConfirmPasswordError" class="login_input_mail_info login_d_none">Die Passwörter stimmen nicht überein.</small>
+        </div>
+        <div class="login_checkbox_container">
+            <div onclick="acceptTerms()" class="login_checkbox">
+                <input type="checkbox" id="acceptTerms" class="form_check_input" disabled>
+            </div> 
+            <div class="login_checkbox">
+                <label for="acceptTerms" class="form-check-label">I accept the <p onclick="showPrivacyPolicy()" class="hover_target">Privacy policy</p></label>
+            </div>
+        </div>
+        <div class="signup_buttons">
+            <button class="login_signup_buttons_styles" id="signUpButton" disabled>
+                Sign up
+            </button>
+        </div>
+    </form>
     `;
 }
