c7f7575bae6474eff7605ffa25e7e953726c79a1
MuhammedTuerkel <muhammedturkel@icloud.com>
2024-12-12 21:15:40 +0100

    Side Navigation extra Dateien hinzugefügt

---
+++ scripts/Side-Navigation.js
@@ -0,0 +1,30 @@
+let User = [];
+
+function Init() {
+    getFromLocalStorage();
+    putName();
+}
+
+function showSubmenu(event) {
+    event.stopPropagation();
+    document.getElementById('Submenu').style = "";
+}
+
+function stopEventBubbling(event) {
+    event.stopPropagation();
+}
+
+function hideSubMenu() {
+    document.getElementById('Submenu').style = "display:none;"
+}
+
+function getFromLocalStorage() {
+    let text = localStorage.getItem("loggedInUser");
+    let Json = JSON.parse(text);
+    User.push(Json);
+}
+
+function putName() {
+    let firstLetter = User[0].name.charAt(0).toUpperCase();
+    document.getElementById('user').innerHTML = `${firstLetter}`;
+}
