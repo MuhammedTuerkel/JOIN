4258a6ceffaea589f78604fd504af501081f27ce
Cici403 <christian.schreter@gmail.com>
2024-12-13 23:19:45 +0100

    added the legal notice  template

--- scripts/privacy_and_legal.js
+++ scripts/privacy_and_legal.js
@@ -14,26 +14,41 @@ function renderPrivacyPolicyTemplateNotLoggedInUser() {
 }
 
 /**
- * when the user hwo is registred want to see the privacy policy.
- * the links Summary, Ad Task, Board, Contacts are to see 
+ * this function is an help function to clear the html and load the Privacy Policy template
  */
-function renderPrivacyPolicyTemplateLoggedInUser(){
-    let newTab = window.open("/privacy-and-legal.html", "_blank");
-    newTab.addEventListener('DOMContentLoaded', function() {
-        function showForregistredUser(){
-            newTab.document.getElementById('privacy').classList.add('activated');
-            newTab.document.getElementById('asideMenu').style.display = 'flex';
-            newTab.loadPrivacyTemplate();
-        }
-        showForregistredUser();
-    });
-}
-
 function loadPrivacyTemplate(){
     let termPolicy = document.getElementById('termsContent');
     termPolicy.innerHTML = "";
     termPolicy.innerHTML += getPrivacyPolicyTemplate();
 }
 
+/**
+ * when the user who is not registered wants to see the privacy policy.
+ * the links Summary, Add Task, Board, Contacts are not to be seen.
+ */
+function renderLegalNoticeTemplateNotLoggedInUser() {
+    let newTab = window.open("/privacy-and-legal.html", "_blank");
+    newTab.addEventListener('DOMContentLoaded', function() {
+        function showNotLoggedInUserAside() {
+            newTab.document.getElementById('privacy').classList.add('activated');
+            newTab.loadLegalTemplate();
+        }
+        showNotLoggedInUserAside();
+    });
+}
 
+/**
+ * this function is an help function to clear the html and load the Privacy Policy template
+ */
+function loadLegalTemplate(){
+    let termPolicy = document.getElementById('termsContent');
+    termPolicy.innerHTML = "";
+    termPolicy.innerHTML += getLegalNoticeTemplate();
+}
 
+/**
+ * close these tab
+ */
+function backToThePage() {
+    window.close();   
+}
--- templates/privacyAndLegalTemplate.js
+++ templates/privacyAndLegalTemplate.js
@@ -69,3 +69,44 @@ function getPrivacyPolicyTemplate(){
                 <p class="pp_contact_details">Developer Akademie<br>Tassiopl 25<br>84541 München Deutschland</p>
     `
 }
+
+function getLegalNoticeTemplate(){
+    return `
+    <div class="privacy_and_legal_privacy_plolicy_text_container">
+        <h1>Imprint</h1>
+        <p>Muhammed T.</p>
+        <p>Markus S.</p>
+        <p>Christian S.</p>
+        <p>Tassiopl 25</p>
+        <p>84541 München Deutschland</p>
+
+        <h2>Exploring the Board</h2>
+        <p>Email: info@developerakademie.com</p>
+    
+        <h2>Acceptance of terms</h2>
+        <p>By accessing and using <span class="pp_highlight">Join</span>, you acknowledge and agree to the following terms and conditions, and any policies, guidelines, or amendments thereto that may be presented to you from time to time. We, the listed students, may update or change the terms and conditions from time to time without notice.</p>
+    
+        <h2>Scope and ownership of the product</h2>
+        <p><span class="pp_highlight">Join</span> has been developed as part of a student group project in a web development bootcamp at the <span class="pp_highlight">Developer Akademie</span>. It has an educational purpose and is not intended for extensive personal & business usage. As such, we cannot guarantee consistent availability, reliability, accuracy, or any other aspect of quality regarding this Product.</p>
+
+        <p>The design of <span class="pp_highlight">Join</span> is owned by the <span class="pp_highlight">Developer Akademie</span>. Unauthorized use, reproduction, modification, distribution, or replication of the design is strictly prohibited.</p>
+    
+        <h2>Proprietary rights</h2>
+        <p>Aside from the design owned by <span class="pp_highlight">Developer Akademie</span>, we, the listed students, retain all proprietary rights in [Product], including any associated copyrighted material, trademarks, and other proprietary information.</p>
+
+        <h2>Use of the product</h2>
+        <p><span class="pp_highlight">Join</span> is intended to be used for lawful purposes only, in accordance with all applicable laws and regulations. Any use of [Product] for illegal activities, or to harass, harm, threaten, or intimidate another person, is strictly prohibited. You are solely responsible for your interactions with other users of <span class="pp_highlight">Join</span>.</p>
+    
+        <h2>Disclaimer of warranties and limitation of liability</h2>
+        <p><span class="pp_highlight">Join</span> is provided "as is" without warranty of any kind, whether express or implied, including but not limited to the implied warranties of merchantability, fitness for a particular purpose, and non-infringement. In no event will we, the listed students, or the <span class="pp_highlight">Developer Akademie</span>, be liable for any direct, indirect, incidental, special, consequential or exemplary damages, including but not limited to, damages for loss of profits, goodwill, use, data, or other intangible losses, even if we have been advised of the possibility of such damages, arising out of or in connection with the use or performance of <span class="pp_highlight">Join</span>.</p>
+    
+        <h2>Indemnity</h2>
+        <p>You agree to indemnify, defend and hold harmless us, the listed students, the <span class="pp_highlight">Developer Akademie</span>, and our affiliates, partners, officers, directors, agents, and employees, from and against any claim, demand, loss, damage, cost, or liability (including reasonable legal fees) arising out of or relating to your use of [Product] and/or your breach of this Legal Notice.</p>
+
+        <p>For any questions or notices, please contact us at info@developerakademie.com</p>
+        <p>Date: July 26, 2023</p>
+        <p class="pp_highlight">Developer Akademie</p>
+        <p class="pp_highlight">Join</p>
+    </div>
+    `
+}
