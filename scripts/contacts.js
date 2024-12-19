741094773dd8c51fbfc3868267f1325b0063a3ea
MuhammedTuerkel <muhammedturkel@icloud.com>
2024-12-19 17:19:14 +0100

    Contact Javascript angefangen

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,7 +1,50 @@
+let Contacts = [
+    [
+        { "name": "Anton Mayer", "email": "antonm@gmail.com" },
+        { "name": "Anja Schulz", "email": "schulz@hotmail.com" }
+    ],
+    [
+        {"name": "Benedikt Ziegler", "email": "benedikt@gmail.com"}
+    ],
+    [],
+    [
+        {"name": "David Eisenberg", "email": "davidberg@gmail.com"}
+    ],
+    [
+        {"name": "Eva Fischer", "email": "eva@gmail.com"},
+        {"name": "Emmanuel Mauer", "email": "emmanuelma@gmail.com"}
+    ],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [
+        {"name": "Marcel Bauer", "email": "bauer@gmail.com"}
+    ],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [
+        {"name": "Tatjana Wolf", "email": "wolf@gmail.com"}
+    ],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+]
+
 function loadContact(i, event) {
     event.stopPropagation();
     if (window.innerWidth < 1050) {
-        document.getElementById('Contacts').style ="display:none;"
+        document.getElementById('Contacts').style = "display:none;"
         document.getElementById('headline-contact').classList.remove("d-none3");
     }
     hideContact();
@@ -13,10 +56,10 @@ function loadContact(i, event) {
     }, 100);
 }
 
-function hideContactMobile(event){
+function hideContactMobile(event) {
     event.stopPropagation();
-    if (window.innerWidth < 1050) { 
-        document.getElementById('Contacts').style ="";
+    if (window.innerWidth < 1050) {
+        document.getElementById('Contacts').style = "";
         document.getElementById('headline-contact').classList.add("d-none3");
     }
     hideContact();
@@ -37,24 +80,24 @@ function loadAddNewContact(event) {
     document.getElementById('addNewContact').classList.remove('d-none');
 }
 
-function hideAddNewContact(event){
+function hideAddNewContact(event) {
     event.stopPropagation();
     document.getElementById('addNewContact').classList.remove('animationRightToPosition');
     document.getElementById('addNewContact').classList.add('d-none');
 }
 
-function stopPropagation(event){
+function stopPropagation(event) {
     event.stopPropagation();
 }
 
-function showEditDeleteMenu(event){
+function showEditDeleteMenu(event) {
     event.stopPropagation();
     document.getElementById('editdelete-menu').classList.remove('d-none');
     document.getElementById('editdelete-menu').classList.add('animationRightToPosition');
 }
 
-function hideEditDeleteMenu(event){
-event.stopPropagation();
-document.getElementById('editdelete-menu').classList.add('d-none');
-document.getElementById('editdelete-menu').classList.remove('animationRightToPosition');
+function hideEditDeleteMenu(event) {
+    event.stopPropagation();
+    document.getElementById('editdelete-menu').classList.add('d-none');
+    document.getElementById('editdelete-menu').classList.remove('animationRightToPosition');
 }
