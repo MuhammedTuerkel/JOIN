fe06699bbd93b599852207a92cba1dcf9e40ca64
Cici403 <christian.schreter@gmail.com>
2024-12-29 22:55:03 +0100

    renamed the eventlistener

--- scripts/add-task.js
+++ scripts/add-task.js
@@ -624,36 +624,3 @@ function checkFormValidity() {
         createTaskButton.disabled = true;
     }
 }
-
-/**
- * Adds event listeners to display and hide error messages for form fields.
- */
-document.addEventListener('DOMContentLoaded', function() {
-    let taskCategory = document.getElementById('task-category');
-    let taskDueDate = document.getElementById('task-due-date');
-    let taskTitle = document.getElementById('task-title');
-
-    taskCategory.addEventListener('focus', function() {
-        document.getElementById('addTaskCategoryErrorInput').style.display = 'block';
-    });
-
-    taskCategory.addEventListener('blur', function() {
-        document.getElementById('addTaskCategoryErrorInput').style.display = 'none';
-    });
-
-    taskDueDate.addEventListener('focus', function() {
-        document.getElementById('addTaskDateErrorInput').style.display = 'block';
-    });
-
-    taskDueDate.addEventListener('blur', function() {
-        document.getElementById('addTaskDateErrorInput').style.display = 'none';
-    });
-
-    taskTitle.addEventListener('focus', function() {
-        document.getElementById('addTaskTitleErrorInput').style.display = 'block';
-    });
-
-    taskTitle.addEventListener('blur', function() {
-        document.getElementById('addTaskTitleErrorInput').style.display = 'none';
-    });
-});
