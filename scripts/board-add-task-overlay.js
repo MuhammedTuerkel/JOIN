42caa4bd4d2b588be98ef60ed89a81ca76cca4bc
Markus Schöberl <schoeby1988@gmail.com>
2025-01-05 18:26:25 +0100

    Implemented Help-Function on board-add-task-overlay.js

--- scripts/board-add-task-overlay.js
+++ scripts/board-add-task-overlay.js
@@ -1,29 +1,48 @@
+async function findFirebaseIdById(targetID) {
+  try {
+    const response = await fetch(BASE_URL + "tasks.json");
+    if (!response.ok) {
+      throw new Error("Error by fetching data from database");
+    }
+    const tasks = await response.json();
+    if (tasks) {
+      for (const [firebaseId, task] of Object.entries(tasks)) {
+        if (task.id === targetID) {
+          return firebaseId;
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error by fetching data:", error);
+    return null;
+  }
+}
 
 /**
  * Closes the overlay, after clicking outside of the Overlay-Ticket
- * 
- * @param {*} event 
+ *
+ * @param {*} event
  */
 function closeOverlayOnClick(event) {
-    if (event.target === document.getElementById('overlayID')) {
-        toggleOverlay();
-    }
+  if (event.target === document.getElementById("overlayID")) {
+    toggleOverlay();
+  }
 }
 
 /**
  * Toggles the Overlay view
  */
 function toggleOverlay() {
-    let overlay = document.getElementById('overlayID');
-    if (overlay.classList.contains('d_none')) {
-        overlay.classList.remove('d_none');
-        overlay.classList.add('d_flex');
-        document.body.style.overflow = "hidden";
-    } else {
-        overlay.classList.remove('d_flex');
-        overlay.classList.add('d_none');
-        document.body.style.overflow = "auto";
-    }
+  let overlay = document.getElementById("overlayID");
+  if (overlay.classList.contains("d_none")) {
+    overlay.classList.remove("d_none");
+    overlay.classList.add("d_flex");
+    document.body.style.overflow = "hidden";
+  } else {
+    overlay.classList.remove("d_flex");
+    overlay.classList.add("d_none");
+    document.body.style.overflow = "auto";
+  }
 }
 
 /**
@@ -35,96 +54,119 @@ function toggleOverlay() {
  * @param {string} prio - The Priority of the ticket
  * @param {string} ticketID - The exact ID which is based on the Firebase-ID
  */
-function showOverlayTicket(category, ticketTitle, ticketDescription, ticketDate, prio, ticketID) {
-    document.getElementById('overlayID').innerHTML = renderOverlayTicket(category, ticketTitle, ticketDescription, ticketDate, prio, ticketID);
-    renderAssignedUsersOverlay(ticketID);
-    renderSubtasksOverlay(ticketID);
+function showOverlayTicket(
+  category,
+  ticketTitle,
+  ticketDescription,
+  ticketDate,
+  prio,
+  ticketID
+) {
+  document.getElementById("overlayID").innerHTML = renderOverlayTicket(
+    category,
+    ticketTitle,
+    ticketDescription,
+    ticketDate,
+    prio,
+    ticketID
+  );
+  renderAssignedUsersOverlay(ticketID);
+  renderSubtasksOverlay(ticketID);
 }
 
 /**
- * 
- * @param {*} ticketID 
- * @param {*} ticketTitle 
- * @param {*} ticketDescription 
- * @param {*} ticketDate 
- * @param {*} prio 
+ *
+ * @param {*} ticketID
+ * @param {*} ticketTitle
+ * @param {*} ticketDescription
+ * @param {*} ticketDate
+ * @param {*} prio
  */
-function editTicket(ticketID, ticketTitle, ticketDescription, ticketDate, prio) {
-    let target = document.getElementById('overlayCard');
-    target.innerHTML = '';
-    target.innerHTML = renderOverlayEditTicket();
-    document.getElementById('task-title-overlay-edit').value = ticketTitle;
-    document.getElementById('task-description-overlay-edit').value = ticketDescription;
-    document.getElementById('task-due-date-overlay-edit').value = ticketDate;
-    setPriorityOnEdit(prio);
+function editTicket(
+  ticketID,
+  ticketTitle,
+  ticketDescription,
+  ticketDate,
+  prio
+) {
+  let target = document.getElementById("overlayCard");
+  target.innerHTML = "";
+  target.innerHTML = renderOverlayEditTicket();
+  document.getElementById("task-title-overlay-edit").value = ticketTitle;
+  document.getElementById("task-description-overlay-edit").value =
+    ticketDescription;
+  document.getElementById("task-due-date-overlay-edit").value = ticketDate;
+  setPriorityOnEdit(prio);
 }
 
 /**
  * Sets the Priority on the Edit-Overlay based on the prio the task has
- * @param {string} prio 
+ * @param {string} prio
  */
 function setPriorityOnEdit(prio) {
-    if(prio === 'urgent') {
-        setUrgent();
-    } else if(prio === 'medium') {
-        setMedium();
-    } else if(prio = 'low') {
-        setLow();
-    }
+  if (prio === "urgent") {
+    setUrgent();
+  } else if (prio === "medium") {
+    setMedium();
+  } else if ((prio = "low")) {
+    setLow();
+  }
 }
 
 /**
  * Sets the Design of the Urgent-Button and temporarily saves the prio into the selectedPrio variable
  */
 function setUrgent() {
-    activateButton('urgent-btn', 'urgent-svg', 'urgent', 'urgent-icon');
-    editedPrio = 'urgent';
+  activateButton("urgent-btn", "urgent-svg", "urgent", "urgent-icon");
+  editedPrio = "urgent";
 }
 
 /**
  * Sets the Design of the Medium-Button and temporarily saves the prio into the selectedPrio variable
  */
 function setMedium() {
-    activateButton('medium-btn', 'medium-svg', 'medium', 'medium-icon');
-    editedPrio = 'medium';
+  activateButton("medium-btn", "medium-svg", "medium", "medium-icon");
+  editedPrio = "medium";
 }
 
 /**
  * Sets the Design of the Low-Button and temporarily saves the prio into the selectedPrio variable
  */
 function setLow() {
-    activateButton('low-btn', 'low-svg', 'low', 'low-icon');
-    editedPrio = 'low';
+  activateButton("low-btn", "low-svg", "low", "low-icon");
+  editedPrio = "low";
 }
 
 /**
  * Based on the clicked Prio-Button, the style of the button is changing. Makes sure, that only one Prio-Button could be active.
- * @param {string} buttonId 
- * @param {string} svgId 
- * @param {string} buttonClass 
- * @param {string} svgClass 
+ * @param {string} buttonId
+ * @param {string} svgId
+ * @param {string} buttonClass
+ * @param {string} svgClass
  */
 function activateButton(buttonId, svgId, buttonClass, svgClass) {
-    const buttons = document.querySelectorAll('.prio-btn');
-    buttons.forEach((button) => {
-        button.classList.remove('urgent', 'medium', 'low', 'active');
-    });
-    const svgs = document.querySelectorAll('svg');
-    svgs.forEach((svg) => {
-        svg.classList.remove('urgent-icon', 'medium-icon', 'low-icon');
-    });
-    document.getElementById(buttonId).classList.add(buttonClass, 'active');
-    document.getElementById(svgId).classList.add(svgClass);
+  const buttons = document.querySelectorAll(".prio-btn");
+  buttons.forEach((button) => {
+    button.classList.remove("urgent", "medium", "low", "active");
+  });
+  const svgs = document.querySelectorAll("svg");
+  svgs.forEach((svg) => {
+    svg.classList.remove("urgent-icon", "medium-icon", "low-icon");
+  });
+  document.getElementById(buttonId).classList.add(buttonClass, "active");
+  document.getElementById(svgId).classList.add(svgClass);
 }
 
 /**
- * 
+ *
  */
 function saveEditonClick() {
-    let newTitle = document.getElementById('task-title-overlay-edit').value;
-    let newDescription = document.getElementById('task-description-overlay-edit').value;
-    let newDueDate = document.getElementById('task-due-date-overlay-edit').value;
-    let newPrio = editedPrio;
-    let newAssignedUser = '';
-    let newSubtasks = '';
+  let newTitle = document.getElementById("task-title-overlay-edit").value;
+  let newDescription = document.getElementById(
+    "task-description-overlay-edit"
+  ).value;
+  let newDueDate = document.getElementById("task-due-date-overlay-edit").value;
+  let newPrio = editedPrio;
+  let newAssignedUser = "";
+  let newSubtasks = "";
 }
