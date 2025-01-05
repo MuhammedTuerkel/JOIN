62a5114d6e0815262959c3ef03f4907f1fda4677
Cici403 <christian.schreter@gmail.com>
2025-01-05 20:26:11 +0100

    added the getUserData function

--- script.js
+++ script.js
@@ -2,66 +2,76 @@ let users = [];
 let tasks = [];
 let loggedInUser = [];
 
-const BASE_URL = "https://join-bbd82-default-rtdb.europe-west1.firebasedatabase.app/";
+const BASE_URL =
+  "https://join-bbd82-default-rtdb.europe-west1.firebasedatabase.app/";
 
 /**
  * Load user data from Firebase Realtime Database into global users array
  */
 async function onloadFunction() {
-    let userResponse = await loadData("users");
-    let userKeyArray = Object.keys(userResponse);
+  let userResponse = await loadData("users");
+  let userKeyArray = Object.keys(userResponse);
 
-    for (let index = 0; index < userKeyArray.length; index++) {
-        let userEntries = Object.values(userResponse[userKeyArray[index]]);
-        for (let entry of userEntries) {
-            users.push(entry);
-        }
+  for (let index = 0; index < userKeyArray.length; index++) {
+    let userEntries = Object.values(userResponse[userKeyArray[index]]);
+    for (let entry of userEntries) {
+      users.push(entry);
     }
-    console.log("Global users array:", users);   
+  }
+  console.log("Global users array:", users);
 }
 
 /**
  * Fetch data from Firebase Realtime Database
- * users is the path to the data in the database 
- * 
+ * users is the path to the data in the database
+ *
  */
 async function loadData(path = "") {
-    let response = await fetch(BASE_URL + path + ".json");
-    let responseToJson = await response.json();
-    return responseToJson;
+  let response = await fetch(BASE_URL + path + ".json");
+  let responseToJson = await response.json();
+  return responseToJson;
 }
 
 /**
  * Pushes a new user into the Firebase Realtime Database.
- * 
- * This function prevents the default form submission behavior, collects the user input data, 
+ *
+ * This function prevents the default form submission behavior, collects the user input data,
  * generates a random color, creates a user data object, and posts this data to Firebase.
  * After successfully adding the user, it loads the login template and shows a registration confirmation.
- * 
+ *
  */
 async function pushNewUserinFireBaseArray(event) {
-    event.preventDefault();
-    let userName = document.getElementById('signUpName').value;
-    let userMail = document.getElementById('signUpInputMail').value;
-    let userPassword = document.getElementById('signUpConfirmInputPassword').value;
-    const color = createRandomColor();
-    const userData = {
-        name: userName,
-        email: userMail,
-        password: userPassword,
-        color: color,
-        createdAt: new Date().toISOString(),
-        Contacts  : [
-            {"name": "First Contact", "email": "firstco@gmail.com", "phone": "1234567890"}
-        ],
-    };
-    try {
-        let response = await postData(`users/${userMail.replace('.', '_')}`, userData);
-        console.log("User successfully added to Realtime Database:", response);
-        createUser();
-    } catch (error) {
-        console.error("Error adding user to Realtime Database:", error);
-    }
+  event.preventDefault();
+  let userName = document.getElementById("signUpName").value;
+  let userMail = document.getElementById("signUpInputMail").value;
+  let userPassword = document.getElementById(
+    "signUpConfirmInputPassword"
+  ).value;
+  const color = createRandomColor();
+  const userData = {
+    name: userName,
+    email: userMail,
+    password: userPassword,
+    color: color,
+    createdAt: new Date().toISOString(),
+    Contacts: [
+      {
+        name: "First Contact",
+        email: "firstco@gmail.com",
+        phone: "1234567890",
+      },
+    ],
+  };
+  try {
+    let response = await postData(
+      `users/${userMail.replace(".", "_")}`,
+      userData
+    );
+    console.log("User successfully added to Realtime Database:", response);
+    createUser();
+  } catch (error) {
+    console.error("Error adding user to Realtime Database:", error);
+  }
 }
 
 /**
@@ -71,14 +81,14 @@ async function pushNewUserinFireBaseArray(event) {
  *  The response data from Firebase.
  */
 async function postData(path = "", data = {}) {
-    let response = await fetch(BASE_URL + path + ".json", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data)
-    });
-    return response.json();
+  let response = await fetch(BASE_URL + path + ".json", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
 }
 
 /**
@@ -87,46 +97,46 @@ async function postData(path = "", data = {}) {
  * returns a random hexadecimal color code.
  */
 function createRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 /**
  * Stores the template type as 'privacyPolicy' in sessionStorage and navigates to the 'privacy-and-legal.html' page.
  */
 function openHelpHtml() {
-    sessionStorage.setItem('previousPage', window.location.href);
-    sessionStorage.setItem('templateType', 'help');
-    window.location.href = 'help.html';
+  sessionStorage.setItem("previousPage", window.location.href);
+  sessionStorage.setItem("templateType", "help");
+  window.location.href = "help.html";
 }
 
 /**
  * Stores the template type as 'privacyPolicy' in sessionStorage and navigates to the 'privacy-and-legal.html' page.
  */
 function renderPrivacyPolicyTemplateLoggedInUser() {
-    sessionStorage.setItem('previousPage', window.location.href);
-    sessionStorage.setItem('templateType', 'privacyPolicy');
-    window.location.href = 'privacy-and-legal.html';
+  sessionStorage.setItem("previousPage", window.location.href);
+  sessionStorage.setItem("templateType", "privacyPolicy");
+  window.location.href = "privacy-and-legal.html";
 }
 
 /**
  * Stores the template type as 'legalNotice' in sessionStorage and navigates to the 'privacy-and-legal.html' page.
  */
 function renderLegalNoticeTemplateLoggedInUser() {
-    sessionStorage.setItem('previousPage', window.location.href);
-    sessionStorage.setItem('templateType', 'legalNotice');
-    window.location.href = 'privacy-and-legal.html';
+  sessionStorage.setItem("previousPage", window.location.href);
+  sessionStorage.setItem("templateType", "legalNotice");
+  window.location.href = "privacy-and-legal.html";
 }
 
 /**
  * load the help html side
  */
-function loadHelpHtml(){
-    changeReturnArrowOnclickFunction();
+function loadHelpHtml() {
+  changeReturnArrowOnclickFunction();
 }
 
 /**
@@ -134,8 +144,9 @@ function loadHelpHtml(){
  * and sets the onclick function for the return arrow.
  */
 function renderPrivacyPolicyTemplate() {
-    document.getElementById('termsContent').innerHTML += getPrivacyPolicyTemplate();
-    changeReturnArrowOnclickFunction();
+  document.getElementById("termsContent").innerHTML +=
+    getPrivacyPolicyTemplate();
+  changeReturnArrowOnclickFunction();
 }
 
 /**
@@ -143,35 +154,35 @@ function renderPrivacyPolicyTemplate() {
  * and sets the onclick function for the return arrow.
  */
 function renderLegalNoticeTemplate() {
-    document.getElementById('termsContent').innerHTML += getLegalNoticeTemplate();
-    changeReturnArrowOnclickFunction();
+  document.getElementById("termsContent").innerHTML += getLegalNoticeTemplate();
+  changeReturnArrowOnclickFunction();
 }
 
 /**
  * Sets up the template rendering process based on the stored template type in sessionStorage.
  * Clears the 'termsContent' element and renders the corresponding template.
  */
-document.addEventListener('DOMContentLoaded', function() {
-    const templateType = sessionStorage.getItem('templateType');
-    let template = document.getElementById('termsContent');
-    
-    if (templateType === 'privacyPolicy') {
-        renderPrivacyPolicyTemplate();
-    } else if (templateType === 'legalNotice') {
-        renderLegalNoticeTemplate();
-    } else if (templateType === 'help'){
-        loadHelpHtml();
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  const templateType = sessionStorage.getItem("templateType");
+  let template = document.getElementById("termsContent");
+
+  if (templateType === "privacyPolicy") {
+    renderPrivacyPolicyTemplate();
+  } else if (templateType === "legalNotice") {
+    renderLegalNoticeTemplate();
+  } else if (templateType === "help") {
+    loadHelpHtml();
+  }
 });
 
 /**
  * Changes the onclick function of the return arrow to call the returnToSessionStoragePage function.
  */
 function changeReturnArrowOnclickFunction() {
-    let image = document.getElementById('returnArrow');
-    if (image) {
-        image.onclick = returnToSessionStoragePage;
-    }
+  let image = document.getElementById("returnArrow");
+  if (image) {
+    image.onclick = returnToSessionStoragePage;
+  }
 }
 
 /**
@@ -179,10 +190,17 @@ function changeReturnArrowOnclickFunction() {
  * If no previous page is found, the user will go to the login page of JOIN.
  */
 function returnToSessionStoragePage() {
-    let previousPage = sessionStorage.getItem('previousPage');
-    if (previousPage) {
-        window.location.href = previousPage;
-    } else {
-        window.location.href = '/login.html';
-    }
+  let previousPage = sessionStorage.getItem("previousPage");
+  if (previousPage) {
+    window.location.href = previousPage;
+  } else {
+    window.location.href = "/login.html";
+  }
+}
+
+function getLoggedInUserData() {
+  let loggedUser = localStorage.getItem("loggedInUser");
+  let UserLogg = JSON.parse(loggedUser);
+
+  console.log(UserLogg);
 }
--- scripts/board.js
+++ scripts/board.js
@@ -9,218 +9,228 @@ let selectedPrioOnBoard;
  * Initialize the board features which should be active at site load
  */
 async function onInit() {
-    await getAllTasks();
-    await renderAllTickets(allTasks);
-     onloadFunction();
+  await getAllTasks();
+  await renderAllTickets(allTasks);
+  onloadFunction();
+  getLoggedInUserData();
 }
 
 /**
  * Gets all tasks which are saved in the firebase realtime database with its Firebase-ID
  */
 async function getAllTasks() {
-    let response = await fetch(BASE_URL + 'tasks' + '.json');
-    let responseAsJSON = await response.json();
-    
-    allTasks = Object.entries(responseAsJSON).map(([id, task]) => {
-        return { firebase_id: id, ...task };
-    });
-}
+  let response = await fetch(BASE_URL + "tasks" + ".json");
+  let responseAsJSON = await response.json();
 
+  allTasks = Object.entries(responseAsJSON).map(([id, task]) => {
+    return { firebase_id: id, ...task };
+  });
+}
 
 /**
  * Checks whether subtasks are present and hides the corresponding containers
- * 
- * @param {int} allSubtasks 
- * @param {string} ticketID 
+ *
+ * @param {int} allSubtasks
+ * @param {string} ticketID
  */
 function displaySubtasks(allSubtasks, ticketID) {
-    let progressBar = document.getElementById(`ticketSubtaskProgressBar_${ticketID}`);
-    let progressCounter = document.getElementById(`ticketSubtaskCounter_${ticketID}`);
-    if(allSubtasks === 0) {
-        progressBar.classList.add('d_none');
-        progressCounter.classList.add('d_none');
-    }
+  let progressBar = document.getElementById(
+    `ticketSubtaskProgressBar_${ticketID}`
+  );
+  let progressCounter = document.getElementById(
+    `ticketSubtaskCounter_${ticketID}`
+  );
+  if (allSubtasks === 0) {
+    progressBar.classList.add("d_none");
+    progressCounter.classList.add("d_none");
+  }
 }
 
 function subtasksClosed(ticketID) {
-    let task = allTasks.find(t => t['id'] === ticketID);
-    if (!task || !task.subtasks) {
-        return 0;
-    }
-    return task.subtasks.filter(st => st['status'] === 'closed').length;
+  let task = allTasks.find((t) => t["id"] === ticketID);
+  if (!task || !task.subtasks) {
+    return 0;
+  }
+  return task.subtasks.filter((st) => st["status"] === "closed").length;
 }
 
 function updateProgressBar(ticketID) {
-    let closedTasks = subtasksClosed(ticketID);
-    let task = allTasks.find(t => t['id'] === ticketID);
-    let totalSubtasks = task && task.subtasks ? task.subtasks.length : 0;
-    const progressPercentage = totalSubtasks > 0 ? (closedTasks / totalSubtasks) * 100 : 0;
-    let progressBar = document.getElementById(`progress-bar_${ticketID}`);
-    if (progressBar) {
-        progressBar.style.width = progressPercentage + '%';
-    }
+  let closedTasks = subtasksClosed(ticketID);
+  let task = allTasks.find((t) => t["id"] === ticketID);
+  let totalSubtasks = task && task.subtasks ? task.subtasks.length : 0;
+  const progressPercentage =
+    totalSubtasks > 0 ? (closedTasks / totalSubtasks) * 100 : 0;
+  let progressBar = document.getElementById(`progress-bar_${ticketID}`);
+  if (progressBar) {
+    progressBar.style.width = progressPercentage + "%";
+  }
 }
 
 function updateSubtaskCounter(ticketID) {
-    let closedTasks = subtasksClosed(ticketID);
-    let task = allTasks.find(t => t['id'] === ticketID);
-    let totalSubtasks = task.subtasks ? task.subtasks.length : 0;
-    let counterElement = document.getElementById(`subtask-counter_${ticketID}`);
-    if (counterElement) {
-        counterElement.textContent = `${closedTasks}/${totalSubtasks}`;
-    }
+  let closedTasks = subtasksClosed(ticketID);
+  let task = allTasks.find((t) => t["id"] === ticketID);
+  let totalSubtasks = task.subtasks ? task.subtasks.length : 0;
+  let counterElement = document.getElementById(`subtask-counter_${ticketID}`);
+  if (counterElement) {
+    counterElement.textContent = `${closedTasks}/${totalSubtasks}`;
+  }
 }
 
 /**
  * Shortens a string and adds some points at the end of the sentence
- * 
+ *
  * @param {string} string - The string which should be shortened
  * @param {int} maxLength - Maximum Length of the text
- * @returns 
+ * @returns
  */
 function shortenDescription(string, maxLength = 44) {
-    if(string.length > maxLength) {
-        return string.slice(0, maxLength - 3) + '...';
-    }
-    return string;
+  if (string.length > maxLength) {
+    return string.slice(0, maxLength - 3) + "...";
+  }
+  return string;
 }
 
 /**
  * Returns the index of the searched Task based on its title in the allTasks-Array
- * 
- * @param {string} title 
+ *
+ * @param {string} title
  * @returns The Index of the searched Task in the allTasks-Array
  */
 function findTicketIndex(title) {
-    let ticketIndex = allTasks.findIndex(ix => ix['title'] === title);
-    return ticketIndex;
+  let ticketIndex = allTasks.findIndex((ix) => ix["title"] === title);
+  return ticketIndex;
 }
 
 /**
  * Changes the Subtask Status in the front-end and on the Firebase DB
- * 
- * @param {int} subtaskIndex 
- * @param {string} ticketID 
+ *
+ * @param {int} subtaskIndex
+ * @param {string} ticketID
  */
 async function changeSubtaskStatus(subtaskIndex, ticketID) {
-    let task = allTasks.find(t => t['id'] === ticketID);
-    let firebaseID = task['firebase_id'];
-    let subtask = task.subtasks[subtaskIndex];
-    subtask.status = subtask.status === 'open' ? 'closed' : 'open';
-    try {
-        await fetch(`${BASE_URL}tasks/${firebaseID}/subtasks/${subtaskIndex}.json`, {
-            method: 'PATCH',
-            body: JSON.stringify({ status: subtask.status }),
-            headers: { 'Content-Type': 'application/json' }
-        });
-    } catch (error) {
-        console.error('Failed to update subtask in Firebase:', error);
-    }
-    renderSubtasksOverlay(ticketID);
-    updateProgressBar(ticketID);
-    updateSubtaskCounter(ticketID);
+  let task = allTasks.find((t) => t["id"] === ticketID);
+  let firebaseID = task["firebase_id"];
+  let subtask = task.subtasks[subtaskIndex];
+  subtask.status = subtask.status === "open" ? "closed" : "open";
+  try {
+    await fetch(
+      `${BASE_URL}tasks/${firebaseID}/subtasks/${subtaskIndex}.json`,
+      {
+        method: "PATCH",
+        body: JSON.stringify({ status: subtask.status }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    console.error("Failed to update subtask in Firebase:", error);
+  }
+  renderSubtasksOverlay(ticketID);
+  updateProgressBar(ticketID);
+  updateSubtaskCounter(ticketID);
 }
 
 /**
  * Saves the ticketID into a variable for further jobs
- * 
+ *
  * @param {string} ticketID - an unique identifier of the ticket which got moved
  */
 function startDragging(ticketID) {
-    currentDraggedElement = `${ticketID}`;
+  currentDraggedElement = `${ticketID}`;
 }
 
 /**
  * Sets the dropzone into a default-mode
- * 
- * @param {*} ev 
+ *
+ * @param {*} ev
  */
 function allowDrop(ev) {
-    ev.preventDefault();
+  ev.preventDefault();
 }
 
 /**
  * Moves the ticket into another column and update the state in the firebase database
- * 
- * @param {string} category - describes in which column the ticket was moved 
+ *
+ * @param {string} category - describes in which column the ticket was moved
  */
 async function move(category) {
-    let currentIndex = allTasks.findIndex(ix => ix['id'] === currentDraggedElement);
-    if(currentIndex !== -1) {
-        allTasks[currentIndex]['state'] = category;
-        let firebaseID = allTasks[currentIndex]['firebase_id'];
-        await fetch(`${BASE_URL}tasks/${firebaseID}.json`, {
-            method: 'PATCH',
-            body: JSON.stringify({state: category}),
-            headers: {'Content-Type': 'application/json'}
-        });
-        renderFilteredTickets();
-    } else {
-        console.error('Task not found in allTasks array')
-    }
+  let currentIndex = allTasks.findIndex(
+    (ix) => ix["id"] === currentDraggedElement
+  );
+  if (currentIndex !== -1) {
+    allTasks[currentIndex]["state"] = category;
+    let firebaseID = allTasks[currentIndex]["firebase_id"];
+    await fetch(`${BASE_URL}tasks/${firebaseID}.json`, {
+      method: "PATCH",
+      body: JSON.stringify({ state: category }),
+      headers: { "Content-Type": "application/json" },
+    });
+    renderFilteredTickets();
+  } else {
+    console.error("Task not found in allTasks array");
+  }
 }
 
 /**
  * Places a dummy ticket into the column which got a dragover
- * 
- * @param {string} id - The id of the column which got a dragover 
+ *
+ * @param {string} id - The id of the column which got a dragover
  */
 function highlight(id) {
-    let column = document.getElementById(id);
-    if(!column.querySelector('.dummy-ticket-card')) {
-        column.innerHTML += renderDummyTicket();
-    } 
+  let column = document.getElementById(id);
+  if (!column.querySelector(".dummy-ticket-card")) {
+    column.innerHTML += renderDummyTicket();
+  }
 }
 
 /**
  * Removes the dummy ticket from the column which got a dragleave
- * 
- * @param {string} id - The id of the column which got a dragleave 
+ *
+ * @param {string} id - The id of the column which got a dragleave
  */
 function removeHighlight(id) {
-    let column = document.getElementById(id);
-    let dummyCard = column.querySelector('.dummy-ticket-card');
-    if(dummyCard) {
-        column.removeChild(dummyCard);
-    }
+  let column = document.getElementById(id);
+  let dummyCard = column.querySelector(".dummy-ticket-card");
+  if (dummyCard) {
+    column.removeChild(dummyCard);
+  }
 }
 
 /**
  * Deletes the task from the board, the array and from the Firebase-DB
- * 
- * @param {string} ticketID 
- * @returns 
+ *
+ * @param {string} ticketID
+ * @returns
  */
 async function deleteTicket(ticketID) {
-    let taskIndex = allTasks.findIndex(task => task['id'] === ticketID);
-    let firebaseID = allTasks[taskIndex]['firebase_id'];
-    try {
-        await fetch(`${BASE_URL}tasks/${firebaseID}.json`, {
-            method: 'DELETE',
-            headers: { 'Content-Type': 'application/json' }
-        });
-    } catch (error) {
-        console.error('Failed to delete ticket from Firebase:', error);
-        return;
-    }
-    allTasks.splice(taskIndex, 1);
-    renderAllTickets(allTasks);
-    toggleOverlay();
+  let taskIndex = allTasks.findIndex((task) => task["id"] === ticketID);
+  let firebaseID = allTasks[taskIndex]["firebase_id"];
+  try {
+    await fetch(`${BASE_URL}tasks/${firebaseID}.json`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Failed to delete ticket from Firebase:", error);
+    return;
+  }
+  allTasks.splice(taskIndex, 1);
+  renderAllTickets(allTasks);
+  toggleOverlay();
 }
 
 /**
- * 
+ *
  */
 function handleSearchInput() {
-    clearTimeout(debounceTimeout);
-    debounceTimeout = setTimeout(() => {
-        let searchTerm = document.getElementById('boardSearchInput').value.trim();
-        if (searchTerm === '') {
-            reloadBoard();
-            return;
-        }
-        startSearch();
-    }, 1000);
+  clearTimeout(debounceTimeout);
+  debounceTimeout = setTimeout(() => {
+    let searchTerm = document.getElementById("boardSearchInput").value.trim();
+    if (searchTerm === "") {
+      reloadBoard();
+      return;
+    }
+    startSearch();
+  }, 1000);
 }
 
 /**
@@ -228,90 +238,95 @@ function handleSearchInput() {
  * @returns Return ends the function if the search input is empty
  */
 function startSearch() {
-    let searchTerm = document.getElementById('boardSearchInput').value.trim();
-    if(searchTerm === '') {
-        reloadBoard();
-        return;
+  let searchTerm = document.getElementById("boardSearchInput").value.trim();
+  if (searchTerm === "") {
+    reloadBoard();
+    return;
+  }
+  clearBoard();
+  let searchResults = [];
+  allTasks.forEach((task) => {
+    const titleMatch = task.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const descriptionMatch = task.description
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    if (titleMatch || descriptionMatch) {
+      searchResults.push(task);
     }
-    clearBoard();
-    let searchResults = [];
-    allTasks.forEach(task => {
-        const titleMatch = task.title.toLowerCase().includes(searchTerm.toLowerCase());
-        const descriptionMatch = task.description.toLowerCase().includes(searchTerm.toLowerCase());
-        if (titleMatch || descriptionMatch) {
-          searchResults.push(task);
-        }
-    });
-    if(searchResults.length === 0) {
-        showFailedSearchMessage(searchTerm);
-        reloadBoard();
-    } else {
-        renderAllTickets(searchResults);
-    }
+  });
+  if (searchResults.length === 0) {
+    showFailedSearchMessage(searchTerm);
+    reloadBoard();
+  } else {
+    renderAllTickets(searchResults);
+  }
 }
 
 /**
  * Clears the Board
  */
 function clearBoard() {
-    document.getElementById('toDoColumn').innerHTML = '';
-    document.getElementById('inProgressColumn').innerHTML = '';
-    document.getElementById('awaitFeedbackColumn').innerHTML = '';
-    document.getElementById('doneColumn').innerHTML = '';
+  document.getElementById("toDoColumn").innerHTML = "";
+  document.getElementById("inProgressColumn").innerHTML = "";
+  document.getElementById("awaitFeedbackColumn").innerHTML = "";
+  document.getElementById("doneColumn").innerHTML = "";
 }
 
 /**
  * Reloads the Board with all tickets
  */
 function reloadBoard() {
-    renderAllTickets(allTasks);
-    document.getElementById('boardSearchInput').value = '';
+  renderAllTickets(allTasks);
+  document.getElementById("boardSearchInput").value = "";
 }
 
 /**
  * Shows the Failed Search Message in an overlay, if the search could not be found on the board
- * @param {string} searchTerm 
+ * @param {string} searchTerm
  */
 function showFailedSearchMessage(searchTerm) {
-    let target = document.getElementById('overlayID');
-    target.classList.remove('d_none');
-    target.innerHTML = renderFailedSearchBox(searchTerm);
+  let target = document.getElementById("overlayID");
+  target.classList.remove("d_none");
+  target.innerHTML = renderFailedSearchBox(searchTerm);
 }
 
 /**
  * open the add task overlay
- * @param {*} state 
+ * @param {*} state
  */
 function showAddTaskOnBoard(state) {
-    let target = document.getElementById('overlayID');
-    target.classList.remove('d_none');
-    target.innerHTML = renderAddTaskFeatureOnBoard(state);
-    addEventListeners();
+  let target = document.getElementById("overlayID");
+  target.classList.remove("d_none");
+  target.innerHTML = renderAddTaskFeatureOnBoard(state);
+  addEventListeners();
 }
 
 /**
  * Adds event listeners to display and hide error messages for form fields.
  */
 function addEventListeners() {
-    let taskCategory = document.getElementById('task-category');
-    let taskDueDate = document.getElementById('task-due-date');
-    let taskTitle = document.getElementById('task-title');
-    taskCategory.addEventListener('focus', function() {
-        document.getElementById('addTaskCategoryErrorInput').style.display = 'block';
-    });
-    taskCategory.addEventListener('blur', function() {
-        document.getElementById('addTaskCategoryErrorInput').style.display = 'none';
-    });
-    taskDueDate.addEventListener('focus', function() {
-        document.getElementById('addTaskDateErrorInput').style.display = 'block';
-    });
-    taskDueDate.addEventListener('blur', function() {
-        document.getElementById('addTaskDateErrorInput').style.display = 'none';
-    });
-    taskTitle.addEventListener('focus', function() {
-        document.getElementById('addTaskTitleErrorInput').style.display = 'block';
-    });
-    taskTitle.addEventListener('blur', function() {
-        document.getElementById('addTaskTitleErrorInput').style.display = 'none';
-    });
+  let taskCategory = document.getElementById("task-category");
+  let taskDueDate = document.getElementById("task-due-date");
+  let taskTitle = document.getElementById("task-title");
+  taskCategory.addEventListener("focus", function () {
+    document.getElementById("addTaskCategoryErrorInput").style.display =
+      "block";
+  });
+  taskCategory.addEventListener("blur", function () {
+    document.getElementById("addTaskCategoryErrorInput").style.display = "none";
+  });
+  taskDueDate.addEventListener("focus", function () {
+    document.getElementById("addTaskDateErrorInput").style.display = "block";
+  });
+  taskDueDate.addEventListener("blur", function () {
+    document.getElementById("addTaskDateErrorInput").style.display = "none";
+  });
+  taskTitle.addEventListener("focus", function () {
+    document.getElementById("addTaskTitleErrorInput").style.display = "block";
+  });
+  taskTitle.addEventListener("blur", function () {
+    document.getElementById("addTaskTitleErrorInput").style.display = "none";
+  });
 }
