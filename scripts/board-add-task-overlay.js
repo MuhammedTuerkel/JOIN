72b3393ee314653fec7d14a66563a7e20292398b
Markus Schöberl <schoeby1988@gmail.com>
2025-01-06 18:59:02 +0100

    Changed Subtask Edit function

--- scripts/board-add-task-overlay.js
+++ scripts/board-add-task-overlay.js
@@ -160,76 +160,34 @@ function activateButton(buttonId, svgId, buttonClass, svgClass) {
   document.getElementById(svgId).classList.add(svgClass);
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /**
  *
  */
-function saveEditonClick() {
-  let newTitle = document.getElementById("task-title-overlay-edit").value;
-  let newDescription = document.getElementById(
-    "task-description-overlay-edit"
-  ).value;
-  let newDueDate = document.getElementById("task-due-date-overlay-edit").value;
-  let newPrio = editedPrio;
-  let newAssignedUser = selectedUsers;
-  console.log(newTitle, newDescription, newDueDate, newPrio, newAssignedUser);
+// function saveEditonClick() {
+//   let newTitle = document.getElementById("task-title-overlay-edit").value;
+//   let newDescription = document.getElementById(
+//     "task-description-overlay-edit"
+//   ).value;
+//   let newDueDate = document.getElementById("task-due-date-overlay-edit").value;
+//   let newPrio = editedPrio;
+//   let newAssignedUser = selectedUsers;
+//   console.log(newTitle, newDescription, newDueDate, newPrio, newAssignedUser);
 
-
-  let task = allTasks.find((t) => t["id"] === ticketID);
-  let firebaseID = task["firebase_id"];
-  let subtask = task.subtasks[subtaskIndex];
-  subtask.status = subtask.status === "open" ? "closed" : "open";
-  try {
-    await fetch(
-      `${BASE_URL}tasks/${firebaseID}/subtasks/${subtaskIndex}.json`,
-      {
-        method: "PATCH",
-        body: JSON.stringify({ status: subtask.status }),
-        headers: { "Content-Type": "application/json" },
-      }
-    )
-};
-}
-
-
-
-
+//   let task = allTasks.find((t) => t["id"] === ticketID);
+//   let firebaseID = task["firebase_id"];
+//   let subtask = task.subtasks[subtaskIndex];
+//   subtask.status = subtask.status === "open" ? "closed" : "open";
+//   try {
+//     await fetch(
+//       `${BASE_URL}tasks/${firebaseID}/subtasks/${subtaskIndex}.json`,
+//       {
+//         method: "PATCH",
+//         body: JSON.stringify({ status: subtask.status }),
+//         headers: { "Content-Type": "application/json" },
+//       }
+//     )
+// }
+// }
 
 function buildTask() {
   let taskTitle = document.getElementById("task-title").value;
@@ -256,15 +214,6 @@ function buildTask() {
   );
 }
 
-
-
-
-
-
-
-
-
-
 function pushEditSubtaskArray(id) {
   let newSubtask = document.getElementById("task-subtasks").value;
   idIndex = allTasks.findIndex((i) => i.id === id);
@@ -277,4 +226,27 @@ function pushEditSubtaskArray(id) {
   idArray.subtasks.push({
     newSubtask,
   });
+}
+
+//FUNKTION --> Bereitstellen des subtasksArrays und updaten des UI
+//Hole mir die Subtasks des Tickets
+//Speichere diese bestehenden Subtasks in die subtasksArray
+//Füge den neuen Subtask zum subtasksArray hinzu
+//Rendere die Subtasks-Liste im Front-End neu
+// ERGEBNIS --> Alle Subtasks inkl. der neuen, sind in einer Array gebündelt, die es bereits gibt. Die UI des Overlay Edit Tickets wird aktualisiert.
+
+function pushEditSubtasksArray(ticketID) {
+  let targetTicket = allTasks.filter((i) => i.id === ticketID);
+  subtasksArray = targetTicket[0].subtasks;
+  let newSubtask = document.getElementById("task-subtasks").value;
+  subtasksArray.push({
+    id: subtasksArray.length + 1,
+    content: newSubtask,
+    status: "open",
+  });
+  newSubtask = "";
+  renderSubtaskList();
+  document.getElementById("iconsContainer").style.visibility = "hidden";
+  document.getElementById("add-subtask-btn").style.visibility = "visible";
+  console.log("TEST", subtasksArray);
 }
