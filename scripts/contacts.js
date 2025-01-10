08194701cdf4f366e1d383f8e15a6b2a65d1b7ce
Cici403 <christian.schreter@gmail.com>
2025-01-10 22:03:59 +0100

    added the random color background on the contacts edit overlay

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -269,7 +269,7 @@ function generateEditNewContactHTML(Badge, i) {
   document.getElementById("addNewContact").classList.remove("d-none");
   document.getElementById("headlineAddContacth2").innerHTML = `Edit contact`;
   document.getElementById("headlineAddContactP").style = "display:none;";
-  document.getElementById("addContactProfilePicture").style = "background:orange;";
+  document.getElementById("addContactProfilePicture").style.backgroundColor = Contacts[i].color;
   document.getElementById("addContactProfilePicture").innerHTML = `<h2>${Badge}</h2>`;
   document.getElementById("input-name").value = `${Contacts[i].name}`;
   document.getElementById("input-email").value = `${Contacts[i].email}`;
