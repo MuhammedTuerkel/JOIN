abb2f5db4ae07b91922c41e02abc7fb00502651e
Cici403 <christian.schreter@gmail.com>
2025-01-11 21:34:02 +0100

    changed the name from the host function

--- host.js
+++ host.js
@@ -1,10 +1,24 @@
+/**
+ * Configuration object for different environments.
+ * Contains the base URLs for development, staging, and production environments.
+ * @type {Object}
+ * @property {string} development - The base URL for the development environment.
+ * @property {string} staging - The base URL for the staging environment, if a second server is used.
+ * @property {string} production - The base URL for the production environment.
+ */
 const CONFIG = {
   development: "http://127.0.0.1:5500",
   staging: "new server if you want to get a second server",
   production: "https://join-401.developerakademie.net/JOIN",
 };
 
-function getBaseURL() {
+/**
+ * Retrieves the base URL based on the current environment.
+ * The function checks the hostname of the current location and returns the corresponding base URL.
+ *
+ * @returns {string} The base URL for the current environment.
+ */
+function getBaseWebsideURL() {
   const hostname = window.location.hostname;
   if (hostname.includes("127.0.0.1") || hostname.includes("localhost")) {
     return CONFIG.development;
