188ff508f8338fe5174fe94639c62dba4a3cdc30
Cici403 <christian.schreter@gmail.com>
2025-01-12 10:45:15 +0100

    cotnact edit function ist final and ready to use

--- scripts/contact-edit.js
+++ scripts/contact-edit.js
@@ -7,30 +7,25 @@
 async function saveEditContact(event, i) {
   event.stopPropagation();
   const userEmail = users[i].email.replace(".", "_");
-
   const userName = document.getElementById("input-name").value;
   const userEmailField = document.getElementById("input-email").value;
   const userPhone = document.getElementById("input-phone").value;
-
   const allUsersResponse = await loadData("users");
-
   let userObject = allUsersResponse[userEmail];
   const userKeys = Object.keys(userObject || {});
-
-  if (!userObject || userKeys.length === 0) {
-    console.error(`User mit E-Mail ${userEmail.replace("_", ".")} nicht in Firebase gefunden.`);
-    return;
-  }
 
   userObject[userKeys[0]].name = userName;
   userObject[userKeys[0]].email = userEmailField;
   userObject[userKeys[0]].phone = userPhone;
-
-  console.log(`Updated user data for email ${userEmail.replace("_", ".")}:`, userObject[userKeys[0]]);
-
   await patchData(`users/${userEmail}`, userObject);
+  reloadNewContactData(event, i);
+}
 
-  console.log(`Benutzer ${userEmail.replace("_", ".")} erfolgreich aktualisiert!`);
+async function reloadNewContactData(event, i) {
+  users = [];
+  await onloadFunction();
+  renderContactInformation(i);
+  hideAddNewContact(event);
 }
 
 async function patchData(path, data) {
